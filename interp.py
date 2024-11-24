9e658b8920622634d5533bf37bafa59a75b43b57
jb <jonathan.d.bostock@gmail.com>
2024-11-24 15:38:29 +0000

    SAE training now works though it does kinda crash vscode. Best to run through terminal directly. SAEs have been trained, testing code is coming .

--- interp.py
+++ interp.py
@@ -2,37 +2,122 @@
 import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader, Dataset
-from transformers import Trainer, TrainingArguments
+from transformers import TrainingArguments
 import os
 from tqdm import tqdm
+import numpy as np
+import pandas as pd
 import json
 
 from utils.training import SMILESTokenizer, SMILESDataset, SMILESTransformer, device, collate_fn
 from utils.device import device
-from utils.interp import ActivationsDataset
+from utils.interp import ActivationsDataset, SAETrainer
 from model import JumpSAE, JumpSAEConfig
 
 def main():
 
-    """
+
     generate_data(
         model_path="results/canonical_model",
-        dataset_path="data/allmolgen_pretrain_data_train.csv",
-        output_path="interp/canonical_activations/",
+        dataset_path="data/allmolgen_pretrain_data_valid.csv",
+        output_path="interp/canonical_activations/validation/",
         batch_size=512
     )
-    """
 
+
+    """
     sae_configs = [
         JumpSAEConfig(input_size=256, hidden_size=4096, target_l0=target_l0, epsilon=1e-2)
-        for target_l0 in [16, 32, 64, 128][0:1]
+        for target_l0 in [16, 32, 64]
     ]
-
     train_saes(
         configs=sae_configs,
         activations_path="interp/canonical_activations/",
         output_path="interp/results/",
     )
+    """
+    """
+    test_saes(
+        sae_paths=["interp/results/4096_16", "interp/results/4096_32", "interp/results/4096_64"],
+        dataset_path="interp/canonical_activations/validation/",
+        csv_path="data/allmolgen_pretrain_data_valid.csv",
+        output_path="interp/results/"
+    )
+    """
+
+def test_saes(
+    sae_paths: list[str],
+    dataset_path: str,
+    csv_path: str,
+    output_path: str
+) -> None:
+    """
+    Test the SAEs on the validation dataset. Records the l0 of the activations and the mse.
+    Also make a record of the highest activating molecules for each SAE feature, and the number of times it was activated.
+
+    Args:
+        sae_paths: Paths to the trained SAEs
+        dataset_path: Path to the validation dataset
+        csv_path: Path to the csv file containing the validation dataset
+        output_path: Path to save the results
+    """
+
+    dataset = ActivationsDataset(dataset_path, data_type="validation")
+    valid_data_df = pd.read_csv(csv_path)
+    for sae_path in sae_paths:
+        sae = JumpSAE.from_pretrained(sae_path)
+
+        mse_list = []
+        l0_list = []
+
+        # Get top 10 highest activating molecules for each feature
+        highest_activating_molecule_indices = [
+            [{"molecule_index": -i, "activation": -1} for i in range(10)]
+            for _ in range(sae.config.hidden_size)]
+        
+        total_activations = np.zeros(sae.config.hidden_size)
+
+        for batch_index, batch in enumerate(dataset):
+            encoding_dict = sae.encode(batch["x"])
+            activations = encoding_dict["activations"].cpu().numpy()
+            mse = torch.mean((activations - batch["x"])**2).item()
+            l0 = torch.mean(torch.sum(encoding_dict["gate_values"], dim=1)).item()
+
+            mse_list.append(mse)
+            l0_list.append(l0)
+
+            ### Keep track of the highest activating molecules
+            for row_index, row in enumerate(activations):
+                molecule_index = batch_index * 512 + row_index
+                for feature_index, feature in enumerate(row):
+                    if feature > highest_activating_molecule_indices[feature_index]["activation"]:
+                        # Add the new molecule to the list
+                        # Then sort the list (highest to lowest)
+                        # Then remove the last element
+                        highest_activating_molecule_indices[feature_index].append({"molecule_index": molecule_index, "activation": feature})
+                        highest_activating_molecule_indices[feature_index].sort(key=lambda x: x["activation"], reverse=True)
+                        highest_activating_molecule_indices[feature_index] = highest_activating_molecule_indices[feature_index][:10]
+
+            total_activations += np.sum(activations, axis=0)
+
+        # Convert highest activating molecules to a dictionary
+        for highest_activating_molecule_list in highest_activating_molecule_indices:
+            for item in highest_activating_molecule_list:
+                item["molecule_index"] = int(item["molecule_index"])
+                item["molecule_smiles"] = valid_data_df.iloc[item["molecule_index"]]["smiles"]
+
+        # Save the results
+        with open(os.path.join(output_path, f"{sae_path}_mse.json"), "w") as f:
+            json.dump({
+                "average_mse": np.mean(mse_list),
+                "average_l0": np.mean(l0_list),
+                "total_activations": total_activations.tolist(),
+                "highest_activating_molecules": highest_activating_molecule_indices
+            }, f)
+
+
+
+
 
 def train_saes(
     configs: list[JumpSAEConfig],
@@ -51,18 +136,18 @@ def train_saes(
 
         training_args = TrainingArguments(
             output_dir=os.path.join(output_path, f"{config.hidden_size}_{config.target_l0}"),
-            num_train_epochs=1,
+            num_train_epochs=5,
             per_device_train_batch_size=1,
             save_steps=1000,
-            learning_rate=1e-5,
-            warmup_ratio=0.2,
+            learning_rate=5e-4,
             weight_decay=0,
-            logging_steps=100,
+            logging_steps=25,
             save_strategy="steps",
-            dataloader_pin_memory=False
+            dataloader_pin_memory=False,
+            max_grad_norm=1.0
         )
 
-        trainer = Trainer(
+        trainer = SAETrainer(
             model=sae,
             args=training_args,
             train_dataset=dataset,
@@ -74,9 +159,6 @@ def train_saes(
         # Save the model
         os.makedirs(os.path.join(output_path, f"{config.hidden_size}_{config.target_l0}"), exist_ok=True)
         sae.save_pretrained(os.path.join(output_path, f"{config.hidden_size}_{config.target_l0}"))
-        json.dump(config.model_dump(), open(os.path.join(output_path, f"{config.hidden_size}_{config.target_l0}", "config.json"), "w"))
-        # Save the training logs
-        trainer.save_logs(os.path.join(output_path, f"{config.hidden_size}_{config.target_l0}", "train_logs.json"))
 
 def generate_data(
         model_path: str,
@@ -105,7 +187,7 @@ def generate_data(
     )
 
     # Generate activations for training set
-    os.makedirs(os.path.join(output_path, "training"), exist_ok=True)
+    os.makedirs(output_path, exist_ok=True)
     # Don't do more than 1024 iterations, each iteration takes ~4s and generates ~30MB of data
     iterations = min(1024, len(dataset) // batch_size)
     for i in tqdm(range(iterations)):
@@ -117,7 +199,7 @@ def generate_data(
                batch["graph_distances"].to(device)
             )["fingerprints"]
 
-        torch.save(output, f"{output_path}/training/batch_{i}.pt")
+        torch.save(output, f"{output_path}/batch_{i}.pt")
 
 if __name__ == "__main__":
     main()
--- utils/interp.py
+++ utils/interp.py
@@ -2,12 +2,17 @@
 
 import os
 import torch
+from torch.utils.data import Dataset
+from transformers import Trainer, TrainingArguments
+from model import JumpSAE
+from typing import Callable
 
-class ActivationsDataset(torch.utils.data.Dataset):
-    def __init__(self, activations_path: str):
+class ActivationsDataset(Dataset):
+    def __init__(self, activations_path: str, data_type: str = "training"):
         self.activations_path = activations_path
-        self.training_path = os.path.join(activations_path, "training")
-        self.batch_files = [f for f in os.listdir(self.training_path) if f.endswith('.pt')]
+        self.data_type = data_type
+        self.data_path = os.path.join(activations_path, data_type)
+        self.batch_files = [f for f in os.listdir(self.data_path) if f.endswith('.pt')]
             
     def __len__(self):
         return len(self.batch_files)
@@ -15,3 +20,27 @@ class ActivationsDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         batch_path = os.path.join(self.training_path, self.batch_files[idx])
         return torch.load(batch_path, weights_only=True)
+
+class SAETrainer(Trainer):
+    """Trainer for Sparse Autoencoders, with custom l0 scheduler"""
+    def __init__(self, *, model: JumpSAE, args: TrainingArguments, train_dataset: ActivationsDataset, data_collator: Callable):
+        super().__init__(
+            model=model,
+            args=args,
+            train_dataset=train_dataset,
+            data_collator=data_collator
+        )
+
+        max_steps = len(train_dataset) * args.num_train_epochs
+
+        self.l0_scheduler = lambda step: min(step / max_steps * 5, 1)
+
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=1):
+        outputs = model(**inputs)
+        mse_loss = outputs.mse_loss
+        l0_loss = outputs.l0_loss
+
+        l0_coefficient = self.l0_scheduler(self.state.global_step)
+        loss = mse_loss + l0_coefficient * l0_loss
+
+        return (loss, outputs) if return_outputs else loss
